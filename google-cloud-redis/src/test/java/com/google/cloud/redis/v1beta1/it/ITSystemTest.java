0c6d4e433409318887099856be6fae427de72db1
Neenu1995 <neenushaji@google.com>
2021-11-15 18:13:44 -0500

    chore: clean up old instances before running IT tests

--- google-cloud-redis/src/test/java/com/google/cloud/redis/v1beta1/it/ITSystemTest.java
+++ google-cloud-redis/src/test/java/com/google/cloud/redis/v1beta1/it/ITSystemTest.java
@@ -19,6 +19,7 @@ package com.google.cloud.redis.v1beta1.it;
 import static org.junit.Assert.assertEquals;
 
 import com.google.cloud.ServiceOptions;
+import com.google.cloud.Timestamp;
 import com.google.cloud.redis.v1beta1.CloudRedisClient;
 import com.google.cloud.redis.v1beta1.CloudRedisSettings;
 import com.google.cloud.redis.v1beta1.Instance;
@@ -27,7 +28,9 @@ import com.google.cloud.redis.v1beta1.LocationName;
 import com.google.cloud.redis.v1beta1.UpdateInstanceRequest;
 import com.google.common.collect.Lists;
 import com.google.protobuf.FieldMask;
+import java.text.ParseException;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
@@ -67,6 +70,8 @@ public class ITSystemTest {
                 .build());
     CloudRedisSettings cloudRedisSettings = cloudRedisSettingsBuilder.build();
     client = CloudRedisClient.create(cloudRedisSettings);
+    /* Clean up old instances that were not deleted. */
+    cleanUpOldInstances();
     /* Creates a Redis instance based on the specified tier and memory size. */
     Instance instance =
         Instance.newBuilder()
@@ -76,6 +81,7 @@ public class ITSystemTest {
             .build();
     client.createInstanceAsync(PARENT, INSTANCE, instance).get();
     LOG.info("redis instance created successfully.");
+
   }
 
   @AfterClass
@@ -84,6 +90,21 @@ public class ITSystemTest {
     client.deleteInstanceAsync(INSTANCE_NAME);
     LOG.info("redis instance deleted successfully.");
     client.close();
+  }
+
+  public static void cleanUpOldInstances() throws ParseException {
+   Calendar calendar = Calendar.getInstance();
+    calendar.add(Calendar.DAY_OF_MONTH, -1);
+    Timestamp cutoff = Timestamp.of(calendar.getTime());
+
+    List<Instance> instances = Lists.newArrayList(client.listInstances(PARENT).iterateAll());
+    for (Instance old_instance : instances) {
+      Timestamp createdAt = com.google.cloud.Timestamp.ofTimeSecondsAndNanos(old_instance.getCreateTime().getSeconds(), old_instance.getCreateTime().getNanos());
+      if (createdAt.compareTo(cutoff) < 0) {
+        client.deleteInstanceAsync(old_instance.getName());
+        LOG.info("redis instance "+ old_instance.getName() + " deleted successfully." );
+      }
+    }
   }
 
   @Test
@@ -97,6 +118,7 @@ public class ITSystemTest {
   public void testListInstances() {
     List<Instance> instances = Lists.newArrayList(client.listInstances(PARENT).iterateAll());
     for (Instance instance : instances) {
+      System.out.println(instance.getName());
       if (INSTANCE_NAME.toString().equals(instance.getName())) {
         assertEquals(TIER, instance.getTier());
         assertEquals(INSTANCE_NAME.toString(), instance.getName());
